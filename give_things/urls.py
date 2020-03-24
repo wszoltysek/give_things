c7aee6797d7b67c09c52549690b79410def3248e
wszoltysek <szoltysek.w@gmail.com>
2020-03-24 12:30:46 +0100

    Templates: Base, index, Form1 configuration with views and urls

--- give_things/urls.py
+++ give_things/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main_app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', LandingPage.as_view()),
+    path('add/', AddDonation.as_view()),
+
 ]
