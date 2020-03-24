04e8b50e7735d15e7a6c7c5fac1299b371db53be
wszoltysek <szoltysek.w@gmail.com>
2020-03-24 13:42:33 +0100

    Donation model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -16,3 +17,17 @@ class Institution(models.Model):
     description = models.CharField(max_length=255)
     type = models.IntegerField(choices=TYPES, default=1)
     categories = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=64)
+    city = models.CharField(max_length=64)
+    zip_code = models.CharField(max_length=6)
+    phone_number = models.IntegerField(max_length=12)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
