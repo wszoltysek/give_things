935f57f72f5f14c6c235fbe9ed3875cc2103c564
wszoltysek <szoltysek.w@gmail.com>
2020-03-24 13:10:39 +0100

    Institution model

--- main_app/models.py
+++ main_app/models.py
@@ -3,3 +3,16 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128)
+
+
+class Institution(models.Model):
+    TYPES = (
+        (1, 'Fundacja'),
+        (2, 'Organizacja pozarządowa'),
+        (3, 'Zbiórka lokalna'),
+    )
+
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=255)
+    type = models.IntegerField(choices=TYPES, default=1)
+    categories = models.ManyToManyField(Category)
