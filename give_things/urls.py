889f23ae3197363dd44690bd7eb58a5cbd4fff8a
wszoltysek <szoltysek.w@gmail.com>
2020-03-24 12:46:53 +0100

    Added: Login and Register templates configuration

--- give_things/urls.py
+++ give_things/urls.py
@@ -20,6 +20,8 @@ from main_app.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LandingPage.as_view()),
+    path('login/', Login.as_view()),
+    path('register/', Register.as_view()),
     path('add/', AddDonation.as_view()),
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -7,6 +7,16 @@ class LandingPage(View):
         return render(request, "index.html")
 
 
+class Login(View):
+    def get(self, request):
+        return render(request, "login.html")
+
+
+class Register(View):
+    def get(self, request):
+        return render(request, "register.html")
+
+
 class AddDonation(View):
     def get(self, request):
         return render(request, "form.html")
