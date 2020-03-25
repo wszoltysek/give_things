cb933b2b055a1760feccbd69efbf0c2df1c3bb32
wszoltysek <szoltysek.w@gmail.com>
2020-03-25 12:38:42 +0100

    templates links configuration

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,21 @@
+from django.db.models import Count
 from django.shortcuts import render
 from django.views import View
+
+from main_app.models import *
 
 
 class LandingPage(View):
     def get(self, request):
-        return render(request, "index.html")
+        donation_count = Donation.objects.\
+            values("quantity").annotate(the_count=Count("quantity"))
+        institution_count = Donation.objects. \
+            values("institution").annotate(the_count=Count("institution"))
+        ctx = {
+            "donation_count": donation_count,
+            "institution_count": institution_count
+        }
+        return render(request, "index.html", ctx)
 
 
 class Login(View):
