240266460f0d7b7777cdaa8383edce80ea9e6024
wszoltysek <szoltysek.w@gmail.com>
2020-06-17 16:04:59 +0200

    quotes bugs fixed

--- main_app/forms.py
+++ main_app/forms.py
@@ -10,16 +10,16 @@ class RegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'password1', 'password2']
+        fields = ["first_name", "last_name", "username", "password1", "password2"]
         widgets = {
-            'first_name': TextInput(attrs={'placeholder': 'Imię'}),
-            'last_name': TextInput(attrs={'placeholder': 'Nazwisko'}),
-            'username': TextInput(attrs={'placeholder': 'Email'}),
-            'password1': TextInput(attrs={'placeholder': 'Hasło'}),
-            'password2': TextInput(attrs={'placeholder': 'Powtórz hasło'})
+            "first_name": TextInput(attrs={"placeholder": "Imię"}),
+            "last_name": TextInput(attrs={"placeholder": "Nazwisko"}),
+            "username": TextInput(attrs={"placeholder": "Email"}),
+            "password1": TextInput(attrs={"placeholder": "Hasło"}),
+            "password2": TextInput(attrs={"placeholder": "Powtórz hasło"})
         }
 
 
 class LoginForm(forms.Form):
-    username = forms.EmailField(label="Email", widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
-    password = forms.CharField(label="Hasło", widget=forms.PasswordInput(attrs={'placeholder': 'Hasło'}))
+    username = forms.EmailField(label="Email", widget=forms.EmailInput(attrs={"placeholder": "Email"}))
+    password = forms.CharField(label="Hasło", widget=forms.PasswordInput(attrs={"placeholder": "Hasło"}))
